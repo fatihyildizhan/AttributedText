2e557364b23ed6e512f1afc5af320143eecd0d48
ibrahim akar <ibrahim.akr@gmail.com>
2015-11-14 23:01:30 +0200

    added kahramanmaras -dev branch.

--- AttributedText/ViewController.swift
+++ AttributedText/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let cityList:[String] = ["Bodrum", "Yalıkavak", "Fethiye", "İzmir", "İstanbul"]
-    let plateList:[String] = ["48", "48", "48", "35", "34"]
-    let capacityList:[String] = ["200K", "50K", "208K", "5M", "16M"]
+    let cityList:[String] = ["Bodrum", "Yalıkavak", "Fethiye", "İzmir", "İstanbul", "Kahramanmaraş"]
+    let plateList:[String] = ["48", "48", "48", "35", "34", "46"]
+    let capacityList:[String] = ["200K", "50K", "208K", "5M", "16M", "1B"]
     
     let attrValueCity:String = "city"
     let attrName:String = "Click"
